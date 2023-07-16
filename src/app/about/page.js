b20cbf56984ac9569cfd6bcc6d00b4b816a88d6f
Joe Panetta <jlpanetta1681@icloud.com>
2023-07-16 01:45:01 -0400

    trying to fix the crashed deplo

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -18,10 +18,10 @@ const About = () => {
           />
           <h2 className="text-3xl font-bold mb-8 shadow-lg">About the Founder</h2>
           <p className="text-lg text-gray-800 mb-8 shadow-md">
-            Hi, I'm Joseph Panetta, the founder of Pathfinders. As a highly skilled Full-Stack Web Developer with over 3 years of experience in tutoring and mentoring, as well as 4 years of professional experience as a website creator, software intern, and Quality assurance engineer, as well as coming from a non-traditional education as well, I am passionate about web application development and helping individuals reach their goals in the field of technology.
+            Hi, I am Joseph Panetta, the founder of Pathfinders. As a highly skilled Full-Stack Web Developer with over 3 years of experience in tutoring and mentoring, as well as 4 years of professional experience as a website creator, software intern, and Quality assurance engineer, as well as coming from a non-traditional education as well, I am passionate about web application development and helping individuals reach their goals in the field of technology.
           </p>
           <p className="text-lg text-gray-800 mb-8 shadow-md">
-            My expertise lies in the MERN stack, which includes Mongo.db, Express.js, React (using Next.js), and Node.js. I also have experience with SQL Server for database management. I continuously stay updated with the latest industry trends and explore cutting-edge technologies and frameworks to deliver exceptional results.
+            My expertise lies in the MERN stack, which includes Mongo.db, Express.js, React using Next.js , and Node.js. I also have experience with SQL Server for database management. I continuously stay updated with the latest industry trends and explore cutting-edge technologies and frameworks to deliver exceptional results.
           </p>
           <p className="text-lg text-gray-800 mb-8 shadow-md">
             I completed my Full-Stack Web Development endorsement at Lambda School in San Francisco, where I gained proficiency in front-end and back-end development, Data Structures and Algorithms, design patterns, and SDLC.
@@ -33,7 +33,7 @@ const About = () => {
             In addition to my professional experience, I have worked as a Front-end Development Instructor at NIIT, delivering engaging curriculum materials and achieving a 100% graduation and employment rate among my cohorts. I have also provided coding mentorship and tutoring through VeroSkills LLC, guiding students in Python, JavaScript, HTML, CSS, React, SQL, Unity, and the Gatsby framework.
           </p>
           <p className="text-lg text-gray-800 mb-8 shadow-md">
-            As the founder and lead engineer of Pathfinders Mentoring and Tutoring Programs, I am dedicated to providing comprehensive educational support to underprivileged students. I have designed and built a web-based platform using React.js, Node.js, Brevo, and Firebase, enabling personalized learning experiences and academic growth. Additionally, I have collaborated with educational experts and potential partners to shape the vision and maximize the program's impact on student success.
+            As the founder and lead engineer of Pathfinders Mentoring and Tutoring Programs, I am dedicated to providing comprehensive educational support to underprivileged students. I have designed and built a web-based platform using React.js, Node.js, Brevo, and Firebase, enabling personalized learning experiences and academic growth. Additionally, I have collaborated with educational experts and potential partners to shape the vision and maximize the programs impact on student success.
           </p>
           <p className="text-lg text-gray-800 mb-8 shadow-md">
             I have actively contributed to open-source projects on GitHub, participated in hackathons, and won top positions, demonstrating my innovation and technical prowess. I actively engage in industry events to stay up-to-date with the latest advancements.
